49d5549d951ba2c60b154880ec6e208cd0902300
杨引 <yin.yang@xiaodetech.com>
2018-10-09 14:19:16 +0800

    init

---
+++ Wreath/Wreath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wreath
+//
+//  Created by 杨引 on 2018/10/9.
+//  Copyright © 2018 Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
